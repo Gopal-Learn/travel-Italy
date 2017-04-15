0013d4ea2c0a4fe44011ed3eafb0d92a58a60526
e <1172597655@qq.com>
2017-04-15 17:08:00 +0800

    做一个小小的项目，关于意大利旅游方面的

---
+++ travel Italy/scripts/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	var left = $(".mapbg1").position().left;
+	var top = $(".mapbg1").position().top;
+	$(".map").css("top",top);
+	$(".map").css("left",left);
+	$(".mappizza1").css("top",top);
+	$(".mappizza1").css("left",left);
+	$(".mappizza2").css("top",top);
+	$(".mappizza2").css("left",left);
+	$(".mappizza3").css("top",top);
+	$(".mappizza3").css("left",left);
+	$(".mappizza4").css("top",top);
+	$(".mappizza4").css("left",left);
+	$(".mappizza5").css("top",top);
+	$(".mappizza5").css("left",left);
+	$(".mappizza6").css("top",top);
+	$(".mappizza6").css("left",left);
+})
+
+// alert(window.innerHeight);
